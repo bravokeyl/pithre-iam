48de14d5ff3e5839d1020337ea5eb893d97d1c22
bravokeyl <bravokeyl@gmail.com>
2018-02-24 16:05:25 +0530

    Export just actions object :rocket:

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// const fs = require('fs');
-const AWS = require('aws-sdk');
-const chalk = require('chalk');
-
-const credentials = new AWS.SharedIniFileCredentials({ profile: 'yoav-2' });
-AWS.config.credentials = credentials;
-const iam = new AWS.IAM();
-
 const { lowerFirst } = require('./lib/util');
 
+// Taken from aws-sdk/apis/iam-2010-05-08.min.json @^2.199.0
 const IAMAPI = require('./lib/iam.json');
-const defaultArgs = require('./lib/defaults.json');
-
 
 const { operations } = IAMAPI;
 const actions = Object.keys(operations);
-// console.log('Total IAM Actions: ', actions.length);
 
 const reqMembers = [];
 const args0 = [];
@@ -75,6 +65,7 @@ const pithreUcActions = {
   miscargs: [],
 };
 
+
 const addtoLevel = (level, method) => {
   switch (level) {
     case 1:
@@ -95,9 +86,9 @@ const addtoLevel = (level, method) => {
   return true;
 };
 
-const levelizeActions = (obj, method, args = []) => {
+const levelizeActions = (obj, umethod, args = []) => {
   const level = args.length || 0;
-  // console.log(method, args)
+  const method = lowerFirst(umethod);
   switch (level) {
     case 0:
       obj.args0.push(method);
@@ -115,7 +106,6 @@ const levelizeActions = (obj, method, args = []) => {
       obj.args4.push(method);
       break;
     default:
-      // console.log("Method", method, "Level:", level)
       obj.miscargs.push(method);
   }
   return true;
@@ -140,8 +130,8 @@ const categorizeActions = (e, type = 'NA', args = []) => {
       miscActions.push(e);
       break;
     default:
-      // console.log("DEFAULT TYPE", e);
       levelizeActions(pithreUcActions, e, args);
+      ucActions.push(e);
   }
 };
 
@@ -158,94 +148,11 @@ actions.forEach((e) => {
   }
 });
 
-// const s = [...new Set(reqMembers)];
-
 const pithreIAM = {
-  args0,
-  args1,
-  args2,
-  args3,
-  args4,
-  miscargs,
-  getActions,
+  list: pithreListActions,
+  get: pithreGetActions,
+  write: pithreWriteActions,
+  misc: pithreMiscActions,
 };
-
-const pithreActions = [
-  pithreListActions,
-  pithreGetActions,
-  pithreWriteActions,
-  pithreMiscActions,
-];
-
-const pithreGetActionsArr =
-Object.keys(pithreGetActions).map(e => pithreGetActions[e].length);
-const pithreListActionsArr =
-Object.keys(pithreListActions).map(e => pithreListActions[e].length);
-const pithreWriteActionsArr =
-Object.keys(pithreWriteActions).map(e => pithreWriteActions[e].length);
-const pithreMiscActionsArr =
-Object.keys(pithreMiscActions).map(e => pithreMiscActions[e].length);
-const pithreUcActionsArr =
-Object.keys(pithreUcActions).map(e => pithreUcActions[e].length);
-// const pithreGetActionsArr = Object.keys(pithreGetActions).map((e)=>pithreGetActions[e].length);
-
-console.log('Total IAM Actions', actions.length, 'categorized into ', [args0.length, args1.length, args2.length, args3.length, args4.length,
-  miscargs.length]);
-console.log('IAM Get actions', getActions.length, 'levelized into ', pithreGetActionsArr);
-console.log('IAM List actions', listActions.length, 'levelized into ', pithreListActionsArr);
-console.log('IAM Write actions', writeActions.length, 'levelized into ', pithreWriteActionsArr);
-console.log('IAM Misc actions', miscActions.length, 'levelized into ', pithreMiscActionsArr);
-console.log('IAM UC actions', ucActions.length, 'levelized into ', pithreUcActionsArr);
-// console.log('Pithre Get Actions', pithreGetActionsArr);
-
-// console.log(pithreIAM);
-// fs.writeFile('lib/defaults.json', JSON.stringify(s), (error) => {
-//   if (error) {
-//     console.log('Error while writing to a file');
-//   }
-// });
-const makeRequest = (method, params, dryrun = true) => {
-  const action = lowerFirst(method);
-  const pParams = {};
-  if (Array.isArray(params)) {
-    params.forEach((p) => {
-      pParams[p] = defaultArgs[p];
-    });
-  }
-  // console.log(action, pParams);
-
-  if (!dryrun) {
-    iam[action](pParams, (err) => {
-      if (err) {
-        console.log('=====');
-        console.log('ERR: ', method);
-        console.log(err.code);
-        console.log(err.message);
-      } else {
-        console.log(chalk.green(action));
-        // fs.writeFile(`bk/${e}.json`, JSON.stringify(data), (error) => {
-        //   if (error) {
-        //     console.log('Error while writing to a file');
-        //   }
-        // });
-      }
-    });
-  }
-};
-console.log('==========================================================');
-// Object.keys(pithreIAM)
-// // getActions
-// .forEach((e) => {
-//   const args = pithreIAM[e];
-//   console.log(e);
-//   console.log(chalk.yellow('Running actions with', e," = ", args.length));
-//   // console.log(args);
-//   console.log('==========================================================');
-//   args.forEach((method) => {
-//     const params = (operations[method] && operations[method].input && operations[method].input.required) ? operations[method].input.required : {};
-//     // makeRequest(method, params, false);
-//   });
-// });
-
 
 module.exports = pithreIAM;
